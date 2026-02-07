2316d8e3cc1bd677a79de14226816976d1970849
Teosork <76114875+Teosork@users.noreply.github.com>
2026-02-07 21:04:52 +0100

    Creating functions for numbers and actions

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const buttons = document.querySelector('#buttons');
 const display = document.querySelector('#display');
 
 let currentValue ='0';
+let previousValue = null;
+let currentOperator = null;
 
 buttons.addEventListener('click', (e) => {
     const btn = e.target.closest('button');
@@ -20,8 +22,36 @@ buttons.addEventListener('click', (e) => {
     display.textContent = currentValue;
 });
 
-function handleNumber() {}
+function handleNumber(number) {
+    if (currentValue === '0' && number !== '.'){
+            currentValue = number;
+    }else if (number === '.' && !currentValue.includes('.')){
+        currentValue += number;
+    }else if (number !== '.') {
+        currentValue += number;
+    }
+}
 
-function handleOperator(){}
+// function handleOperator(operator){
+//     previousValue = Number(currentValue);
+//     currentValue = '0';
+//     currentOperator = operator;
+// }
 
-function handleAction(){}
+function handleAction(action){
+    if (action === 'clear') {
+    currentValue = '0';
+    } else if (action === 'backspace'){
+    currentValue = currentValue.slice(0, -1);
+        if (currentValue === '' || currentValue === '-') {     // if empty after backspace
+        currentValue = '0';
+        } 
+    } else if (action === 'toggle-sign'){
+        if (currentValue === '0') return;
+        if (currentValue.startsWith('-')){
+        currentValue = currentValue.slice(1); 
+        } else {
+        currentValue = '-' + currentValue;
+        }
+    }
+}
